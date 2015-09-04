1374dd635f22b35c0a06433c3e4c8814b2c3cd53
cielavenir <cielartisan@gmail.com>
2015-09-05 02:11:33 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@ set -e
 if [ -d EasyTetherLoader.app ]; then
 	curl -O http://www.mobile-stream.com/beta/easytether.dmg
 	7z e easytether.dmg 0.hfs
-	7z e 0.hfs EasyTether/EasyTetherUSBEthernet.pkg/Contents/Archive.pax.gz
+	if [ -f 0.hfs ]; then
+		7z e 0.hfs EasyTether/EasyTetherUSBEthernet.pkg/Contents/Archive.pax.gz
+	else
+		7z e easytether.dmg EasyTether/EasyTetherUSBEthernet.pkg/Contents/Archive.pax.gz
+	fi
 	tar xzf Archive.pax.gz -C EasyTetherLoader.app/Contents/Resources/
-	rm easytether.dmg 0.hfs Archive.pax.gz
+	rm -f easytether.dmg 0.hfs Archive.pax.gz
 	osacompile -o EasyTetherLoader.app/Contents/Resources/Scripts/main.scpt EasyTetherLoader.applescript
 	echo "Successfully installed EasyTetherUSBEthernet.kext"
 else
